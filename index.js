d4d24cfb8047392b433c4a9dd163ca71f2094586
Rocky Haque <rockyhaque99@gmail.com>
2024-09-09 19:10:40 +0600

    authentication done with jwt and nodemailer

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const nodemailer = require("nodemailer");
 const jwt = require("jsonwebtoken");
 const cookieParser = require("cookie-parser");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
@@ -19,25 +20,63 @@ app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
 
+// send email with nodemailer
+const sendEmail = (emailAddress, emailData) => {
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false, // Use `true` for port 465, `false` for all other ports
+    auth: {
+      user: process.env.TRANSPORTER_EMAIL,
+      pass: process.env.TRANSPORTER_PASS,
+    },
+  });
+
+  // verify transporter connection configuration
+  transporter.verify(function (error, success) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Server is ready to take our messages");
+    }
+  });
+
+  const mailBody = {
+    from: `"Neighbourly" <${process.env.TRANSPORTER_EMAIL}>`, // sender address
+    to: emailAddress, // list of receivers
+    subject: emailData.subject, // Subject line
+    html: emailData.message, // html body
+  };
+
+  transporter.sendMail(mailBody, (error, info) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Email Sent: " + info.response);
+    }
+  });
+};
+
 // verify jwt middleware
-const verifyToken = (req, res, next) => {
+const verifyToken = async (req, res, next) => {
   const token = req.cookies?.token;
+  // console.log(token);
   if (!token) {
-    return res.status(401).send({ message: "Unauthorized Access!" });
+    return res.status(401).send({ message: "unauthorized access" });
   }
-  if (token) {
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
-      if (error) {
-        return res.status(401).send({ message: "Unauthorized Access!" });
-      }
-      req.user = decoded;
-      next();
-    });
-  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      console.log(err);
+      return res.status(401).send({ message: "unauthorized access" });
+    }
+    req.user = decoded;
+    next();
+  });
 };
 
 //--------------------- database connection-----------------------
-//Todo: Change the cluster name
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@revive.2tkcldw.mongodb.net/?retryWrites=true&w=majority&appName=Revive`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -51,16 +90,15 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // database and collection
+    const database = client.db("neighbourlyDB");
+    const userCollection = database.collection("users");
 
-    //Todo: database and collection
-
-    // JWT Generate
+    // auth related api
     app.post("/jwt", async (req, res) => {
       const user = req.body;
       const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-        expiresIn: "7d",
+        expiresIn: "365d",
       });
       res
         .cookie("token", token, {
@@ -72,27 +110,59 @@ async function run() {
     });
 
     // clear token with logout
-    app.get("/logout", (req, res) => {
-      res
-        .clearCookie("token", {
-          httpOnly: true,
-          secure: process.env.NODE_ENV === "production",
-          sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
-          maxAge: 0,
-        })
-        .send({ success: true });
+    app.get("/logout", async (req, res) => {
+      try {
+        res
+          .clearCookie("token", {
+            maxAge: 0,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+          })
+          .send({ success: true });
+        console.log("Logout successful");
+      } catch (err) {
+        res.status(500).send(err);
+      }
     });
 
-    //Todo: Create your API here
+    //--------------Main APIs here--------------
 
+    // save a user data
+    app.put("/user", async (req, res) => {
+      const user = req.body;
+      const query = { email: user?.email };
 
+      // check if user already exists in db
+      const isExist = await userCollection.findOne(query);
+      if (isExist) {
+        if (user?.status === "Requested") {
+          // if existing user try to change his role
+          const result = await userCollection.updateOne(query, {
+            $set: { status: user?.status },
+          });
+          return res.send(result);
+        } else {
+          // if existing user login again
+          return res.send(isExist);
+        }
+      }
 
-
-
-
-
-
-    
+      // save user for the first time
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          ...user,
+          timestamp: Date.now(),
+        },
+      };
+      const result = await userCollection.updateOne(query, updateDoc, options);
+      // welcome new user
+      sendEmail(user?.email, {
+        subject: "Welcome to Neighbourly",
+        message: `Reliable Workers, Right at Your Doorstep. Thanks 🌼`,
+      });
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
